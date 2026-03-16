8f30c1b0a186d66f7a7e825de4517efdc1e724a1
nmnlv <ghg38846@gmail.com>
2026-03-17 01:28:55 +0500

    Create base_class.py

---
+++ backend/app/db/base_class.py
@@ -0,0 +1,44 @@
+# backend/app/db/base_class.py
+from typing import Any, Dict, Generic, List, Optional, TypeVar
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+
+ModelType = TypeVar("ModelType", bound=Any)
+CreateSchemaType = TypeVar("CreateSchemaType", bound=Any)
+UpdateSchemaType = TypeVar("UpdateSchemaType", bound=Any)
+
+Base = declarative_base()
+
+class CRUDBase(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(self, model: Any):
+        self.model = model
+
+    def get(self, db: Session, id: Any) -> Optional[ModelType]:
+        return db.query(self.model).filter(self.model.id == id).first()
+
+    def get_multi(self, db: Session, skip: int = 0, limit: int = 100) -> List[ModelType]:
+        return db.query(self.model).offset(skip).limit(limit).all()
+
+    def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
+        db_obj = self.model(**obj_in.dict())
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def update(
+        self, db: Session, *, db_obj: ModelType, obj_in: UpdateSchemaType
+    ) -> ModelType:
+        obj_data = obj_in.dict()
+        for field, value in obj_data.items():
+            if value is not None:
+                setattr(db_obj, field, value)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def remove(self, db: Session, *, id: int) -> ModelType:
+        obj = db.query(self.model).get(id)
+        db.delete(obj)
+        db.commit()
+        return obj
