c3d5a760c5d34d728a6ee7823222875a042b8a76
nmnlv <ghg38846@gmail.com>
2026-03-17 01:28:27 +0500

    Create auth.py

---
+++ backend/app/schemas/auth.py
@@ -0,0 +1,11 @@
+# backend/app/schemas/auth.py
+from pydantic import BaseModel
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
